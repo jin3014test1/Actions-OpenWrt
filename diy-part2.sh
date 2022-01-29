ebd93a38e8b0efd548e00e4072f1978a7b6e1636
jin3014test1 <98628465+jin3014test1@users.noreply.github.com>
2022-01-29 10:43:35 +0800

    Update diy-part2.sh
    add OpenAppFilter

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,4 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter  
