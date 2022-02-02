9ebdf4b1804b717816017fe202952a9126e71b89
jin3014test1 <98628465+jin3014test1@users.noreply.github.com>
2022-02-02 20:28:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,5 +12,5 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
-# git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 git clone https://github.com/romantices/luci-app-smartdns-lede.git package/luci-app-smartdns
