8db5d19237a45a24a1c0eb46a60aab35a3588bc0
jin3014test1 <98628465+jin3014test1@users.noreply.github.com>
2022-02-03 13:34:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,5 +12,5 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
-git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+# git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
