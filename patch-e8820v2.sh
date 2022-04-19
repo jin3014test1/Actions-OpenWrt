dc87e8becd03597b0e825f4308d8f6a5d97e69a0
jin3014test2 <100070878+jin3014test2@users.noreply.github.com>
2022-04-19 16:42:57 +0800

    Update patch-e8820v2.sh

--- patch-e8820v2.sh
+++ patch-e8820v2.sh
@@ -137,8 +137,7 @@ sed -i 's/"0:lan:4" "1:lan:3" "2:lan:2" "3:lan:1" "4:wan:5" "6@eth0"/"0:lan:1" "
 #增加驱动
 
 sed -i '$a define Device/zte_e8820v2\
-  $(Device/dsa-migration)\
-  $(Device/uimage-lzma-loader)\
+  
   IMAGE_SIZE := 16064k\
   DEVICE_VENDOR := ZTE\
   DEVICE_MODEL := E8820V2\
