54b1b1950e37bc638a50dbe8944ca675ce287265
jin3014test1 <98628465+jin3014test1@users.noreply.github.com>
2023-12-21 22:36:17 +0800

    Create diy-part2-v21.02.3.sh

---
+++ diy-part2-v21.02.3.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+# sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
+# git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+# git clone -b lede https://github.com/jin3014test1/luci-app-smartdns.git package/luci-app-smartdns
+# git clone --depth=1 https://github.com/fw876/helloworld.git package/helloworld
+git clone https://github.com/siwind/openwrt-vlmcsd package/vlmcsd
+git clone https://github.com/siwind/luci-app-vlmcsd.git package/luci-app-vlmcsd
+git clone https://github.com/siwind/luci-app-usb_printer.git package/luci-app-usb_printer
+
+
+
+# smartdns
+#cd openwrt
+#sudo rm -rf feeds/luci/applications/luci-app-smartdns
+#sudo rm -rf package/feeds/luci/luci-app-smartdns
+#sudo rm -rf feeds/packages/net/smartdns
+#sudo rm -rf package/feeds/packages/smartdns
+#svn co https://github.com/kenzok8/small-package/trunk/luci-app-smartdns package/diy/luci-app-smartdns
+#svn co https://github.com/kenzok8/small-package/trunk/smartdns package/diy/smartdns
