976b1239b00e9ee51cc7edee9010184988c3a0e4
jin3014test1 <98628465+jin3014test1@users.noreply.github.com>
2024-07-27 20:44:19 +0800

    Update diy-part2-master.sh

--- diy-part2-master.sh
+++ diy-part2-master.sh
@@ -23,7 +23,7 @@ sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_genera
 # 加入E8820S支持
 wget https://raw.githubusercontent.com/jin3014test1/Actions-OpenWrt/main/code.tar.gz
 tar -zxvf code.tar.gz
-rsync -r code/tree/* .
+rsync -a code/tree/* .
 #顺手把补丁删了
 rm -rf  code code.tar.gz
 
